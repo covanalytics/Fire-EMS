6e3dc82409bb101ea7d27713c31a2f6b90af279a
covanalytics <tsink@covingtonky.gov>
2017-03-08 15:11:41 -0500

    Add files via upload

--- training.R
+++ training.R
@@ -1,17 +1,18 @@
 
 setwd("U:/CityWide Performance/CovStat/CovStat Projects/Fire/Training")
+library("plyr")
 
 #### Load Update ####
-update <- read.csv("Jan2017.csv", header=TRUE, stringsAsFactors = FALSE)
+update <- read.csv("Update.csv", header=TRUE, stringsAsFactors = FALSE)
 
 #### SQLite storage ####
 library("RSQLite")
 cons.fire <- dbConnect(drv=RSQLite::SQLite(), dbname="O:/AllUsers/CovStat/Data Portal/repository/Data/Database Files/Fire.db")
-dbWriteTable(cons.fire, "FireTraining", update, append = TRUE)
+dbWriteTable(cons.fire, "FireTraining", update, overwrite = TRUE)
 
 ## Load Database ---------------------------
 dash_train <- dbGetQuery(cons.fire, 'select * from FireTraining')
-write.csv(dash_train, "U:/CityWide Performance/CovStat/CovStat Projects/Fire/TableauFiles/PoliceTraining.csv")
+write.csv(dash_train, "U:/CityWide Performance/CovStat/CovStat Projects/Fire/TableauFiles/FireTraining.csv", row.names = FALSE)
 dbDisconnect(cons.fire)
 
 #### Write to CovStat Repository ####
